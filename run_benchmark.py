21d77e83ada2764c88ee1750f52c3033897e8b23
Andy Watkins <andy.watkins2@gmail.com>
2019-07-13 13:40:02 -0700

    Adding final phenix with a minimum of python3 reversions

--- run_benchmark.py
+++ run_benchmark.py
@@ -58,6 +58,8 @@ def erraser2(d: str, execution_fn: Callable, nstruct: int) -> None:
         the_cifs = [c for c in the_cifs if c != 'H2U.cif']
         warning('replacing automatically generated H2U parameters')
         # provide from somewhere.
+        #os.system('cp {}/h2u_two_plane.cif ./output/'.format(os.path.dirname(os.path.realpath(__file__))))
+        os.system('cp {}/h2u_two_plane.cif ./'.format(os.path.dirname(os.path.realpath(__file__))))
         the_cifs.append('h2u_two_plane.cif')
 
     ###
@@ -66,11 +68,13 @@ def erraser2(d: str, execution_fn: Callable, nstruct: int) -> None:
         try:
             # We also do the initial phenix refinement once.
             os.system('phenix.refine {} {} {} --overwrite '.format('./output/phenix_updated.pdb', the_mtz, " ".join(the_cifs)))
-            os.rename('phenix_updated_refine_001.pdb'.format(the_pdb.replace('.pdb', '')), './output/phenix_rd1.pdb')
+            #os.rename('phenix_updated_refine_001.pdb'.format(the_pdb.replace('.pdb', '')), './output/phenix_rd1.pdb')
+            os.system('cp phenix_updated_refine_001.pdb ./output/phenix_rd1.pdb')
         except FileNotFoundError:
             warning("didn't find the phenix.refine output file, assuming bad rfree flags")
             os.system('phenix.refine {} {} {} refinement.input.xray_data.r_free_flags.generate=True --overwrite'.format('./output/phenix_updated.pdb', the_mtz, " ".join(the_cifs)))
-            os.rename('phenix_updated_refine_001.pdb'.format(the_pdb.replace('.pdb', '')), './output/phenix_rd1.pdb')
+            #os.rename('phenix_updated_refine_001.pdb'.format(the_pdb.replace('.pdb', '')), './output/phenix_rd1.pdb')
+            os.system('cp phenix_updated_refine_001.pdb ./output/phenix_rd1.pdb')
     
     # Phase 2.1 -- optionally, IF we are setting up jobs, copy this critical file to the runs dirs.
     if nstruct != 0:
@@ -119,7 +123,7 @@ def erraser2(d: str, execution_fn: Callable, nstruct: int) -> None:
 #################
 #memory per node; default is 4000 MB per CPU
 # Consider more for big maps??
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=8000
 #################
  
 cd {pwd}\n\n""".format(the_pdb=the_pdb, dirnum=i, pwd=os.getcwd()))
@@ -131,27 +135,33 @@ cd {pwd}\n\n""".format(the_pdb=the_pdb, dirnum=i, pwd=os.getcwd()))
     # Phase 2.5 -- cull ions and stuff from PDB. How should we do this?
     # Useful approach -- just get rid of most common stuff that will make
     # our lives the worst for the least gain.
-    execution_fn("cat ./output/phenix_rd1.pdb | grep -v MG | grep -v SR > ./output/phenix_rd1_culled.pdb")
+    execution_fn("cat ./output/phenix_rd1.pdb | grep -v HOH | grep -v MG |grep -v SO4| grep -v 'K     K' | grep -v SR > ./output/phenix_rd1_culled.pdb")
 
-    execution_fn("pdb2fasta.py ./output/phenix_rd1_culled.pdb > {}".format(the_pdb.replace('pdb', 'fasta')))
+    # AMW TODO: figure out a system where we aren't constantly overwriting this if we fix it in one instance.
+    execution_fn("if [[ ! -e ./output/{} ]] ; then\n\tpdb2fasta.py ./output/phenix_rd1_culled.pdb > ./output/{}\nfi\n".format(the_pdb.replace('pdb', 'fasta'), the_pdb.replace('pdb', 'fasta')))
     ###
     # Phase 3 -- run erraser2 on a culled portion of the phenix pdb.
     #  
-    if not os.path.exists('./output/erraser_rd1.pdb'):
-        try:
-            exe: str = '/Users/amw579/jd3_dev/main/source/bin/erraser2'
-            score_flags: str = ' -score:weights stepwise/rna/rna_res_level_energy4.wts -restore_talaris_behavior'
-            weight_adj: str = ' -set_weights elec_dens_fast 10.0 cart_bonded 5.0 linear_chainbreak 10.0 chainbreak 10.0 fa_rep 1.5 fa_intra_rep 0.5 rna_torsion 10 suiteness_bonus 5 rna_sugar_close 10 atom_pair_constraint 10 '
-            mutes: str = '-mute  core.scoring.electron_density.xray_scattering core.pack.rotamer_set.RotamerSet_ core.scoring.CartesianBondedEnergy -render_density true'
-            mapfile: str = '{}_2mFo-DFc_map.ccp4'.format(the_pdb.replace('.pdb', ''))
-            fasta: str = the_pdb.replace('pdb', 'fasta')
-            #-sampler_num_pose_kept 10 for quick?
-            execution_fn('{} -s ./output/phenix_rd1_culled.pdb {} {} {} -rmsd_screen 3.0 -edensity:mapfile {} -fasta {} -allow_virtual_side_chains false -sampler_num_pose_kept 10'.format(
-                exe, score_flags, weight_adj, mutes, mapfile, fasta
-            ))
-            execution_fn('mv ./output/phenix_rd1_culledFINISHED.pdb ./output/erraser_rd1.pdb')
-        except:
-            quit()
+    try:
+        exe= '/home/groups/rhiju/amw579/Rosetta/main/source/bin/erraser2.default.linuxgccrelease' #'$Gsource/bin/erraser2'
+        score_flags= ' -score:weights stepwise/rna/rna_res_level_energy4.wts -restore_talaris_behavior'
+        weight_adj= ' -set_weights elec_dens_fast 10.0 cart_bonded 5.0 linear_chainbreak 10.0 chainbreak 10.0 fa_rep 1.5 fa_intra_rep 0.5 rna_torsion 10 suiteness_bonus 5 rna_sugar_close 10 atom_pair_constraint 10 '
+        mutes= '-mute  core.scoring.electron_density.xray_scattering core.pack.rotamer_set.RotamerSet_ core.scoring.CartesianBondedEnergy -render_density true'
+        mapfile= '{}_2mFo-DFc_map.ccp4'.format(the_pdb.replace('.pdb', ''))
+        fasta= the_pdb.replace('pdb', 'fasta')
+        #-sampler_num_pose_kept 10 for quick?
+        execution_fn('if [[ ! -e ./output/erraser_rd1.pdb ]]; then \n\tif [[ ! -e ./output/phenix_rd1_culled.pdbFINISHED_1.pdb ]] ; then\n\t\t{} -s ./output/phenix_rd1_culled.pdb {} {} {} -rmsd_screen 3.0 -edensity:mapfile {} -fasta ./output/{} -allow_virtual_side_chains false -sampler_num_pose_kept 10 -missing_density_to_jump true\n\tfi\n\n\tmv ./output/phenix_rd1_culled.pdbFINISHED_1.pdb ./output/erraser_rd1.pdb\nfi\n'.format(
+            exe, score_flags, weight_adj, mutes, mapfile, fasta
+        ))
+    except:
+        quit()
+
+    try:
+        # We also do the initial phenix refinement once.
+        execution_fn('if [[ ! -e ./output/phenix_rd2.pdb ]] ; then \n\tcp {} ./output/\n\tcp *.cif ./output/\n\tpushd ./output/\n\tphenix.refine {} {} {} --overwrite\n\tmv erraser_rd1_refine_001.pdb phenix_rd2.pdb\n\tpopd\nfi'.format(the_mtz, 'erraser_rd1.pdb', the_mtz, " ".join(the_cifs)))
+    except:
+        quit()
+            
     
     os.chdir('..')
 
